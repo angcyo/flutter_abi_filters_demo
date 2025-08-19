bccd3c72ca4d03cdec04d0749175609813d474e3
angcyo <angcyo@126.com>
2025-08-19 11:36:50 +0800

    + abi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,10 @@ android {
         versionName = flutter.versionName
 
         ndk {
-            abiFilters.add("arm64-v8a")
+            //abiFilters.clear()
+            //abiFilters.add("arm64-v8a")
+            //abiFilters.add("armeabi-v7a")
+            abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
         }
     }
 
